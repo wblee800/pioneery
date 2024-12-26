18df6601223e757e95a88272c2f00f3189c29cac
Wonbin Lee <wblee800@gmail.com>
2024-12-26 17:16:25 -0600

    Add Components to the project

---
+++ frontend/build/static/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Board from "./Board"
+import PostForm from "./PostForm"
+import PostList from "./PostList"
+
+const App = () => {
+  <h1>This is the App.jsx</h1>
+};
