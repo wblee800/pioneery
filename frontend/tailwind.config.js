d9fa41cb657460e6316a3036d7d13897e02001b7
June Kim <junho85@gmail.com>
2025-01-05 21:08:34 +0900

    Add MapViewer component with Google Maps integration
    Introduced the MapViewerTest component featuring Google Maps to display and interact with company locations. Updated dependencies for map functionality, added Tailwind CSS, and configured dotenv for managing API keys. Updated webpack and project structure for compatibility with new features.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // 또는 프로젝트 구조에 맞는 경로
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
